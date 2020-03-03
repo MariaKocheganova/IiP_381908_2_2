b3d4b1a98771f9e0b550d1544694b58b757c110e
Sinyakin.AS <endermix@yandex.ru>
2020-03-03 17:10:02 +0300

    Task1.Variant2.Version2

--- sinyakin.as/Task1/Main.cpp
+++ sinyakin.as/Task1/Main.cpp
@@ -1,14 +1,14 @@
 #include "Header.h"
 #include <iostream>
 
+
 using namespace std;
 
 int main()
 {
-	setlocale(LC_CTYPE, "Russian");
 
 	CString str1; // конструктор по умолчанию
-	CString str2("Нижний Новгород"); // конструктор преобразования типа
+	CString str2("Nizhny Novgorod"); // конструктор преобразования типа
 	CString str3 (10, '*'); // конструктор инициализатор
 	CString str4 (15); // конструктор инициализатор
 	CString str5 (str2); // конструктор копирования
@@ -22,14 +22,14 @@ int main()
 
 	str3 = str2;
 	str3.OutPut("str3");
-	str2 = " - Российский город";
+	str2 = " - is a russian city";
 	str2.OutPut("str2");
 	str5 = str3 + str2;
 	str5.OutPut("str5");
 	str3 += str2;
-	str3 = "Нижний Новгород";
+	str3 = "Nizhny Novgorod";
 	str3.OutPut("str3");
-	str3 += " - Наш любимый город!";
+	str3 += " - it's the greatest city!";
 	s3.outPut("s3");
 
 	return 0;
