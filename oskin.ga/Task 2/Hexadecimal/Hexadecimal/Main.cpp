0cfba17c25db27b16ea5bc7f98899a6ffcdcba56
OskinGA <42718711+Oskg@users.noreply.github.com>
2020-04-17 21:48:15 +0300

    Hex class

---
+++ oskin.ga/Task 2/Hexadecimal/Hexadecimal/Main.cpp
@@ -0,0 +1,21 @@
+#include "Hex.h"
+#include <Windows.h>
+int main(){
+
+	Hex a("FFFF"); // 65535
+	Hex b(); // 0
+	Hex c("A"); //10
+	Hex e(c);
+	Hex d = a - c;
+	cout << d << endl;
+	Hex m("AB");
+	cout << a - m << endl;
+	if (a > m) cout << "+";
+	if (a < m) cout << "-";
+	if (c == e) cout << "equal" << endl;
+	Hex j;
+	cin >> j;
+	cout << j << endl; 
+	system("pause");
+	return 0;
+}
