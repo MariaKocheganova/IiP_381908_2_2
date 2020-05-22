41fe9382023546551036c84219f1957a53dfadd1
OskinGA <42718711+Oskg@users.noreply.github.com>
2020-05-22 22:14:39 +0300

    -

---
+++ oskin.ga/Task 3/Menu.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include <conio.h>
+using namespace std;
+
+class Menu
+{
+	
+public:
+	Menu(int pun, string* name);
+	Menu(string file);
+	Menu();
+};
+
---
+++ oskin.ga/Task 3/Source.cpp
@@ -0,0 +1,12 @@
+#include "Menu.h"
+
+int main() {
+	string puncts[4];
+	puncts[0] = "Odin";
+	puncts[1] = "Dva";
+	puncts[2] = "Tri";
+	puncts[3] = "Chetire";
+	int pun = 4;
+	Menu(pun,puncts);
+	return 0;
+}
