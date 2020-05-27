0158375b80b4b5cdacf3d6c37a19f5439f69b911
Shilov.RI <roma11nn2@mail.ru>
2020-05-27 17:30:45 +0300

    Zadanie2

---
+++ zadanie 2/zadanie 2.cpp
@@ -0,0 +1,32 @@
+﻿#include "Octa.h"
+
+int main() {
+
+	Octal a;
+	cin >> a;
+	Octal b;
+	cin >> b;
+	Octal c, f;
+	cout << "a =  ";
+	cout << a;
+	cout << "b =  ";
+	cout << b;
+	cout << "a - b= ";
+	c = a - b;
+	cout << c;
+	cout << "a + b= ";
+	c = a + b;
+	cout << c;
+
+	if (a == b) {
+		cout << " a ravno b";
+	}
+	else {
+		cout << "a ne ravno b";
+	}
+
+	getchar();
+	getchar();
+	return 0;
+}
+//сначала 1 (длина числа), потом число(само число), затем 2 (второе число) и число 
