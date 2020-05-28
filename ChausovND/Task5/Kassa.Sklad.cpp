e84d4c2faceb08c12efa0a646c1cc791da396202
ChausovND <61248768+ChausovND@users.noreply.github.com>
2020-05-28 04:00:54 +0300

    Task5. Variant1 Version1

---
+++ ChausovND/Task5/Kassa.Sklad.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <windows.h>
+using namespace std;
+
+class Sklad
+{
+public:
+	string returnName(int i)
+	{
+		return name[i];
+	}
+	int returnPrice(int i)
+	{
+		return price[i];
+	}
+	void newItem()
+	{
+		int brcd2, price2;
+		string name2;
+		cout << "-- New catalog entry --\n";
+		cout << "Enter barcode: "; cin >> brcd2;
+		brcd.push_back(brcd2);
+		cout << "Enter name: "; cin >> name2;
+		name.push_back(name2);
+		cout << "Enter price: "; cin >> price2;
+		price.push_back(price2);
+	}
+	bool search(int brcd4)
+	{
+		for (int i = 0; i < brcd.size(); i++)
+			if (brcd4 == brcd[i])
+				return true;
+		return false;
+	}
+
+private:
+	vector <int> brcd; vector <string>name; vector <int> price;
+};
+
+class Kassa
+{
+public:
+	void menu()
+	{
+		int menu;
+		bool flag = true;
+		while (flag)
+		{
+			cout << "----- Choose an action -----" << endl;
+			cout << "1) Add a new item to the catalog..." << endl;
+			cout << "2) Choose an item to buy..." << endl;
+			cout << "3) Display cheque" << endl;
+			cout << "4) Display item description..." << endl;
+			cout << "5) Remove the last item from the cheque" << endl;
+			cout << "6) Exit" << endl;
+			cin >> menu;
+
+			switch (menu)
+			{
+			case 1:
+				wrhs.newItem();
+				break;
+			case 2:
+			{
+				int brcd3;
+				cout << "----- Cash register -----\n";
+				cout << "Enter barcode: "; cin >> brcd3;
+				menu = wrhs.search(brcd3);
+
+				if (menu == false)
+					cout << "Found no item with such barcode" << endl;
+				else
+				{
+					int menu2;
+					cout << "Item found and added to the cheque" << endl;
+					cin >> menu2;
+					cheque.push_back(menu);
+				}
+				break;
+			}
+			case 3:
+				formCheque();
+				break;
+			case 6:
+				flag = false;
+				break;
+			}
+		}
+	}
+	void formCheque()
+	{
+		int	 total = 0;
+		if (cheque.size() == 0)
+			cout << "The cheque is empty" << endl;
+		else
+		{
+			int q = 1;
+			sort(cheque.begin(), cheque.end());
+			cheque.push_back(9999);
+			cout << "----- The cheque -----" << endl;
+			for (int i = 1; i < cheque.size(); i++)
+			{
+				if (cheque[i - 1] != cheque[i])
+				{
+					cout << "Name: " << wrhs.returnName(cheque[i - 1]) << endl;
+					cout << "Price: " << wrhs.returnPrice(cheque[i - 1]) << endl;
+					cout << "Quantity: " << q << endl;
+					total += wrhs.returnPrice(cheque[i - 1]) * q;
+					cout << "--------------------" << endl;
+					q = 1;
+				}
+				else
+					q++;
+			}
+			cout << "Total: " << total << endl;
+			cheque.pop_back();
+		}
+	}
+
+private:
+	Sklad wrhs;
+	vector <int> cheque;
+};
+
+int main()
+{
+	Kassa K;
+	K.menu();
+
+	system("pause");
+	return 0;
+}
